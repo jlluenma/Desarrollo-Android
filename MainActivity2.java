a38f540e64e8f024f4af88d3ead1f798ed3122b5
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-06-25 15:23:28 -0500

    Update MainActivity2.java

--- MainActivity2.java
+++ MainActivity2.java
@@ -1,14 +1,21 @@
-package com.jlluenma.myapplication;
+package com.cixteam.balance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity2 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String userInput = intent.getStringExtra("userInput");
+
+        TextView userTextView = findViewById(R.id.userTextView);
+        userTextView.setText(userInput);
     }
 }
