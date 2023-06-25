1499f932ce9495250ce49763e6858d0ec5303f59
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-06-25 15:21:33 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,52 +1,38 @@
-package com.jlluenma.myapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+package com.cixteam.balance;
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
-
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button btnOpenActivity = findViewById(R.id.btnOpenActivity);
-        btnOpenActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
-
-                EditText editText = findViewById(R.id.nameUsr);
-                String nombreUsuario = editText.getText().toString();
-            }
-        });
-
-    }
-}
-version 2 ############################
-package com.cixteam.balance;
-
-import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.EditText;
+
 
 //import com.google.android.gms.fonts.Fonts;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameUsr;
+    private String userInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
       //  Fonts.initialize(getApplicationContext());
+        nameUsr = findViewById(R.id.nameUsr);
+
+        Button btnOpenActivity = findViewById(R.id.btnOpenActivity);
+        btnOpenActivity.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+            userInput = nameUsr.getText().toString();
+
+            intent = new Intent(MainActivity.this, MainActivity2.class);
+            intent.putExtra("userInput", userInput);
+            startActivity(intent);
+
+        });
     }
 }
