e2a308b9716290a9065b39cbcd3abb20707f91a8
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-06-24 19:23:55 -0500

    Update and rename MainActivity.kt to MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.jlluenma.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnOpenActivity = findViewById(R.id.btnOpenActivity);
+        btnOpenActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+        EditText editText = findViewById(R.id.nameUsr);
+        String nombreUsuario = editText.getText().toString();
+
+    }
+}
