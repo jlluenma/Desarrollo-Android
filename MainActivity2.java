c2cd2a45feb9d738e38c2e293803149e2aad9879
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-07-03 18:46:28 -0500

    Update MainActivity2.java

--- MainActivity2.java
+++ MainActivity2.java
@@ -2,6 +2,8 @@ package com.cixteam.balance;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,5 +19,16 @@ public class MainActivity2 extends AppCompatActivity {
 
         TextView userTextView = findViewById(R.id.userTextView);
         userTextView.setText(userInput);
+
+        Button btnOpenStock = findViewById(R.id.btnOpenStock);
+        btnOpenStock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir la nueva actividad (MainActivity3)
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
