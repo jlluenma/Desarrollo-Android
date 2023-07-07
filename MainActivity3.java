6b93cfa6c11946baf3116650225a1daa00ce6226
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-07-07 16:01:26 -0500

    Update MainActivity3.java

--- MainActivity3.java
+++ MainActivity3.java
@@ -1,14 +1,44 @@
 package com.cixteam.balance;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.cixteam.balance.databinding.ActivityMain3Binding;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
 
 public class MainActivity3 extends AppCompatActivity {
+
+    ActivityMain3Binding binding;
+
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() == null){
+            Toast.makeText(this, "CANCELADO", Toast.LENGTH_SHORT).show();
+        } else {
+            binding.etCodigo.setText(result.getContents());
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+       binding = ActivityMain3Binding.inflate(getLayoutInflater());
+       setContentView(binding.getRoot());
+        binding.btnScanner.setOnClickListener(view -> escanear());
+    }
+    public void escanear(){
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES);
+        options.setPrompt("ESCANEAR CÓDIGO");
+        options.setCameraId(0);
+        options.setOrientationLocked(false);
+        options.setBeepEnabled(true);
+        options.setCaptureActivity(CaptureActivityPortrait.class);
+        options.setBarcodeImageEnabled(false);
+        barcodeLauncher.launch(options);
     }
 }
