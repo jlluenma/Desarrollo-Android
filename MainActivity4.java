f34fd64d09e3f66707181dc4620843208906008d
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-07-14 02:44:39 -0500

    Update MainActivity4.java

--- MainActivity4.java
+++ MainActivity4.java
@@ -26,13 +26,38 @@ public class MainActivity4 extends AppCompatActivity {
         if (scannedResult == null) {
             Toast.makeText(this, "CANCELADO", Toast.LENGTH_SHORT).show();
         } else {
-            // Realizar la consulta a la tabla en DbHelper utilizando el resultado escaneado
-            // Aquí puedes llamar a un método en el DbHelper para realizar la consulta
-            // Pasar la variable scannedResult como argumento para la consulta
-            // Por ejemplo:
-            // dbConsulta(scannedResult);
+            // Realizar la consulta a la tabla TABLE_PRODUCTS en el DbHelper
+            DbHelper dbHelper = new DbHelper(this);
+            String nombre = "";
+            String tipo = "";
+
+            // Realizar la consulta y obtener los datos correspondientes al código escaneado
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            String[] projection = {"nombre", "tipo"};
+            String selection = "code = ?";
+            String[] selectionArgs = {scannedResult};
+            Cursor cursor = db.query(DbHelper.TABLE_PRODUCTS, projection, selection, selectionArgs, null, null, null);
+
+            // Verificar si el cursor contiene algún resultado
+            if (cursor.moveToFirst()) {
+                // Obtener los datos (nombre y tipo)
+                nombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"));
+                tipo = cursor.getString(cursor.getColumnIndexOrThrow("tipo"));
+            }
+
+            cursor.close();
+            db.close();
+
+            // Realizar alguna acción con los datos obtenidos (nombre y tipo) en la actividad 4
+            // Puedes pasar los datos a la actividad 4 utilizando un Intent o almacenarlos en variables globales
+            // Ejemplo de cómo pasar los datos a la actividad 4:
+            Intent intent = new Intent(MainActivity4.this, MainActivity6.class);
+            intent.putExtra("nombreProducto", nombre);
+            intent.putExtra("tipoProducto", tipo);
+            startActivity(intent);
         }
     });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
