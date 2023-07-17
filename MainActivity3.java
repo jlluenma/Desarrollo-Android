dd8a80ea71f804966279f774a5af843894a31688
Jorge Lluen Madrid <81448319+jlluenma@users.noreply.github.com>
2023-07-17 16:53:39 -0500

    Update MainActivity3.java

--- MainActivity3.java
+++ MainActivity3.java
@@ -3,46 +3,57 @@ package com.cixteam.balance;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.cixteam.balance.databinding.ActivityMain3Binding;
+import com.cixteam.balance.db.DbHelper;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
 import android.content.Intent;
-import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity3 extends AppCompatActivity {
 
-    ActivityMain3Binding binding;
-
     private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(), result -> {
         if (result.getContents() == null){
             Toast.makeText(this, "CANCELADO", Toast.LENGTH_SHORT).show();
         } else {
-            binding.etCodigo.setText(result.getContents());
+            String scannedValue = result.getContents();
+
+            DbHelper dbHelper = new DbHelper(this);
+            SQLiteDatabase database = dbHelper.getReadableDatabase();
+
         }
     });
+
+    private DbHelper dbHelper;
+    private SQLiteDatabase database;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       binding = ActivityMain3Binding.inflate(getLayoutInflater());
-       setContentView(binding.getRoot());
-        binding.btnScanner.setOnClickListener(view -> escanear());
+        setContentView(R.layout.activity_main3);
+
+        dbHelper = new DbHelper(this);
+        database = dbHelper.getReadableDatabase();
+
+        Button btnScanner = findViewById(R.id.btnScanner);
+        btnScanner.setOnClickListener(view -> escanear());
 
         Button btnList = findViewById(R.id.btnList);
         btnList.setOnClickListener(v -> {
-            // Abrir la nueva actividad (MainActivity5)
             Intent intent = new Intent(MainActivity3.this, MainActivity5.class);
             startActivity(intent);
         });
-
-
     }
+
     public void escanear(){
         ScanOptions options = new ScanOptions();
         options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES);
@@ -54,4 +65,33 @@ public class MainActivity3 extends AppCompatActivity {
         options.setBarcodeImageEnabled(false);
         barcodeLauncher.launch(options);
     }
+
+    private void obtenerDatosProducto(String scannedValue) {
+        String[] columns = {"nombre", "tipo"};
+        String selection = "code = ?";
+        String[] selectionArgs = {scannedValue};
+
+        Cursor cursor = database.query(DbHelper.TABLE_PRODUCTS, columns, selection, selectionArgs, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            String nombreProducto = getString(cursor.getColumnIndex("nombre"));
+            String tipoProducto = getString(cursor.getColumnIndex("tipo"));
+            TextView textViewNombreProducto = findViewById(R.id.editTextNombreProducto);
+            TextView textViewTipoProducto = findViewById(R.id.editTextTipoProducto);
+
+            textViewNombreProducto.setText(nombreProducto);
+            textViewTipoProducto.setText(tipoProducto);
+        }
+
+        cursor.close();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        dbHelper.close();
+        database.close();
+    }
+    
+    
 }
